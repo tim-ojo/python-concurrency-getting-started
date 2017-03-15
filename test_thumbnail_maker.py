f7bf17add84d67067dffc95aca0c77d8f036af42
Tim Ojo <ojotim@gmail.com>
2017-03-15 07:18:36 +0100

    reset repo

--- test_thumbnail_maker.py
+++ test_thumbnail_maker.py
@@ -1,33 +1,34 @@
 from thumbnail_maker import ThumbnailMakerService
 
+IMG_URLS = \
+    ['https://dl.dropboxusercontent.com/s/2fu69d8lfesbhru/pexels-photo-48603.jpeg',
+     'https://dl.dropboxusercontent.com/s/zch88m6sb8a7bm1/pexels-photo-134392.jpeg',
+     'https://dl.dropboxusercontent.com/s/lsr6dxw5m2ep5qt/pexels-photo-135130.jpeg',
+     'https://dl.dropboxusercontent.com/s/6xinfm0lcnbirb9/pexels-photo-167300.jpeg',
+     'https://dl.dropboxusercontent.com/s/2dp2hli32h9p0y6/pexels-photo-167921.jpeg',
+     'https://dl.dropboxusercontent.com/s/fjb1m3grcrceqo2/pexels-photo-173125.jpeg',
+     'https://dl.dropboxusercontent.com/s/56u8p4oplagc4bp/pexels-photo-185934.jpeg',
+     'https://dl.dropboxusercontent.com/s/2s1x7wz4sdvxssr/pexels-photo-192454.jpeg',
+     'https://dl.dropboxusercontent.com/s/1gjphqnllzm10hh/pexels-photo-193038.jpeg',
+     'https://dl.dropboxusercontent.com/s/pcjz40c8pxpy057/pexels-photo-193043.jpeg',
+     'https://dl.dropboxusercontent.com/s/hokdfk7y8zmwe96/pexels-photo-207962.jpeg',
+     'https://dl.dropboxusercontent.com/s/k2tk2co7r18juy7/pexels-photo-247917.jpeg',
+     'https://dl.dropboxusercontent.com/s/m4xjekvqk4rksbx/pexels-photo-247932.jpeg',
+     'https://dl.dropboxusercontent.com/s/znmswtwhcdbpc10/pexels-photo-265186.jpeg',
+     'https://dl.dropboxusercontent.com/s/jgb6n4esquhh4gu/pexels-photo-302899.jpeg',
+     'https://dl.dropboxusercontent.com/s/rjuggi2ubc1b3bk/pexels-photo-317156.jpeg',
+     'https://dl.dropboxusercontent.com/s/cpaog2nwplilrz9/pexels-photo-317383.jpeg',
+     'https://dl.dropboxusercontent.com/s/16x2b6ruk18gji5/pexels-photo-320007.jpeg',
+     'https://dl.dropboxusercontent.com/s/xqzqzjkcwl52en0/pexels-photo-322207.jpeg',
+     'https://dl.dropboxusercontent.com/s/frclthpd7t8exma/pexels-photo-323503.jpeg',
+     'https://dl.dropboxusercontent.com/s/7ixez07vnc3jeyg/pexels-photo-324030.jpeg',
+     'https://dl.dropboxusercontent.com/s/1xlgrfy861nyhox/pexels-photo-324655.jpeg',
+     'https://dl.dropboxusercontent.com/s/v1b03d940lop05d/pexels-photo-324658.jpeg',
+     'https://dl.dropboxusercontent.com/s/ehrm5clkucbhvi4/pexels-photo-325520.jpeg',
+     'https://dl.dropboxusercontent.com/s/l7ga4ea98hfl49b/pexels-photo-333529.jpeg',
+     'https://dl.dropboxusercontent.com/s/rleff9tx000k19j/pexels-photo-341520.jpeg'
+    ]
+    
 def test_thumbnail_maker():
     tn_maker = ThumbnailMakerService()
-    img_urls = \
-    ['https://cdn.rawgit.com/tim-ojo/python-concurrency-getting-started/53382895/SampleImages/10062929553_dcf4406299_k.jpg',
-     'https://cdn.rawgit.com/tim-ojo/python-concurrency-getting-started/53382895/SampleImages/10119289865_a488098f09_z.jpg',
-     'https://cdn.rawgit.com/tim-ojo/python-concurrency-getting-started/53382895/SampleImages/10119493095_5c610b4f96_k.jpg',
-     'https://cdn.rawgit.com/tim-ojo/python-concurrency-getting-started/53382895/SampleImages/10233612245_117aea1c26_z.jpg',
-     'https://cdn.rawgit.com/tim-ojo/python-concurrency-getting-started/53382895/SampleImages/11409439386_58232f1680_k.jpg',
-     'https://cdn.rawgit.com/tim-ojo/python-concurrency-getting-started/53382895/SampleImages/12665166284_c7e6d9c6b3_o.jpg',
-     'https://cdn.rawgit.com/tim-ojo/python-concurrency-getting-started/53382895/SampleImages/13896217830_90c1a48fb2_o.jpg',
-     'https://cdn.rawgit.com/tim-ojo/python-concurrency-getting-started/53382895/SampleImages/14128995140_c4882c9a45_o.jpg',
-     'https://cdn.rawgit.com/tim-ojo/python-concurrency-getting-started/53382895/SampleImages/14537579172_07a90b01e6_o.jpg',
-     'https://cdn.rawgit.com/tim-ojo/python-concurrency-getting-started/53382895/SampleImages/14567056019_b8793a34e3_o.jpg',
-     'https://cdn.rawgit.com/tim-ojo/python-concurrency-getting-started/53382895/SampleImages/14575556067_3645683f90_o.jpg',
-     'https://cdn.rawgit.com/tim-ojo/python-concurrency-getting-started/53382895/SampleImages/14592808598_80a7ca2b07_o.jpg',
-     'https://cdn.rawgit.com/tim-ojo/python-concurrency-getting-started/53382895/SampleImages/15304823795_5fffb5ed9b_o.jpg',
-     'https://cdn.rawgit.com/tim-ojo/python-concurrency-getting-started/53382895/SampleImages/15624035681_6864cbed1e_o.jpg',
-     'https://cdn.rawgit.com/tim-ojo/python-concurrency-getting-started/53382895/SampleImages/19024000380_fb77b7cf66_o.jpg',
-     'https://cdn.rawgit.com/tim-ojo/python-concurrency-getting-started/53382895/SampleImages/19338595355_aa13caf91e_b.jpg',
-     'https://cdn.rawgit.com/tim-ojo/python-concurrency-getting-started/53382895/SampleImages/19352834342_8f43699229_b.jpg',
-     'https://cdn.rawgit.com/tim-ojo/python-concurrency-getting-started/53382895/SampleImages/20292931565_6ceecddf21_b.jpg',
-     'https://cdn.rawgit.com/tim-ojo/python-concurrency-getting-started/53382895/SampleImages/21786078395_8864e1d6b7_b.jpg',
-     'https://cdn.rawgit.com/tim-ojo/python-concurrency-getting-started/53382895/SampleImages/21890494139_0d4cf26353_b.jpg',
-     'https://cdn.rawgit.com/tim-ojo/python-concurrency-getting-started/53382895/SampleImages/22562959415_87302fbfcd_b.jpg',
-     'https://cdn.rawgit.com/tim-ojo/python-concurrency-getting-started/53382895/SampleImages/9590919201_23d6577955_o.jpg',
-     'https://cdn.rawgit.com/tim-ojo/python-concurrency-getting-started/53382895/SampleImages/9633815280_c87a2f90bf_o.jpg',
-     'https://cdn.rawgit.com/tim-ojo/python-concurrency-getting-started/53382895/SampleImages/9650593016_b2533863be_o.jpg',
-     'https://cdn.rawgit.com/tim-ojo/python-concurrency-getting-started/53382895/SampleImages/9923140974_25c02decda_o.jpg',
-     'https://cdn.rawgit.com/tim-ojo/python-concurrency-getting-started/53382895/SampleImages/9923272796_4f35d92941_k.jpg']
-    
-    tn_maker.make_thumbnails(img_urls)
+    tn_maker.make_thumbnails(IMG_URLS)
--- thumbnail_maker.py
+++ thumbnail_maker.py
@@ -4,81 +4,73 @@ import os
 import logging
 from urllib.parse import urlparse
 from urllib.request import urlretrieve
+
 import PIL
-from PIL import Image, ImageOps
+from PIL import Image
 
 logging.basicConfig(filename='logfile.log', level=logging.DEBUG)
 
 class ThumbnailMakerService(object):
     def __init__(self, home_dir='.'):
         self.home_dir = home_dir
+        self.input_dir = self.home_dir + os.path.sep + 'incoming'
+        self.output_dir = self.home_dir + os.path.sep + 'outgoing'
 
     def download_images(self, img_url_list):
-        dest_dir = self.home_dir + os.path.sep + 'incoming'
-        if not os.path.exists(dest_dir):
-            os.mkdir(dest_dir)
+        # validate inputs
+        if not img_url_list:
+            return
+        os.makedirs(self.input_dir, exist_ok=True)
         
+        logging.info("beginning image downloads")
+
         start = time.perf_counter()
         for url in img_url_list:
-            parsed_url = urlparse(url)
-            filename = parsed_url.path.split('/')[-1]
-            urlretrieve(url, dest_dir + os.path.sep + filename)
+            # download each image and save to the input dir 
+            img_filename = urlparse(url).path.split('/')[-1]
+            urlretrieve(url, self.input_dir + os.path.sep + img_filename)
         end = time.perf_counter()
 
         logging.info("downloaded {} images in {} seconds".format(len(img_url_list), end - start))
 
-    def perform_resizing(self, posterize=False):
-        source_dir = self.home_dir + os.path.sep + 'incoming'
-        dest_dir = self.home_dir + os.path.sep + 'outgoing'
-        if not os.path.exists(dest_dir):
-            os.mkdir(dest_dir)
-        target_sizes = [48, 96, 120, 300]
+    def perform_resizing(self):
+        # validate inputs
+        if not os.listdir(self.input_dir):
+            return
+        os.makedirs(self.output_dir, exist_ok=True)
+
+        logging.info("beginning image resizing")
+        target_sizes = [32, 64, 200]
+        num_images = len(os.listdir(self.input_dir))
 
         start = time.perf_counter()
-        for filename in os.listdir(source_dir):
+        for filename in os.listdir(self.input_dir):
+            orig_img = Image.open(self.input_dir + os.path.sep + filename)
             for basewidth in target_sizes:
-                img = Image.open(source_dir + os.path.sep + filename)
-                
+                img = orig_img
+                # calculate target height of the resized image to maintain the aspect ratio
                 wpercent = (basewidth / float(img.size[0]))
                 hsize = int((float(img.size[1]) * float(wpercent)))
-                img = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
+                # perform resizing
+                img = img.resize((basewidth, hsize), PIL.Image.LANCZOS)
                 
+                # save the resized image to the output dir with a modified file name 
                 new_filename = os.path.splitext(filename)[0] + \
                     '_' + str(basewidth) + os.path.splitext(filename)[1]
-                img.save(dest_dir + os.path.sep + new_filename)
+                img.save(self.output_dir + os.path.sep + new_filename)
 
-                if posterize:
-                    edited_image = ImageOps.autocontrast(img, 15)
-                    edited_image = ImageOps.posterize(edited_image, 2)
-                    edited_image = edited_image.point(lambda p: p * 1.2)
-                    edited_image = ImageOps.expand(edited_image, 20, 'white')
-                    edited_filename = os.path.splitext(filename)[0] + \
-                    '_' + str(basewidth) + '_p' + os.path.splitext(filename)[1]
-                    edited_image.save(dest_dir + os.path.sep + edited_filename)
+            os.remove(self.input_dir + os.path.sep + filename)
         end = time.perf_counter()
-        logging.info("created thumbnails in {} seconds".format(end - start))
+
+        logging.info("created {} thumbnails in {} seconds".format(num_images, end - start))
 
     def make_thumbnails(self, img_url_list):
+        logging.info("START make_thumbnails")
+        start = time.perf_counter()
+
         self.download_images(img_url_list)
         self.perform_resizing()
+
+        end = time.perf_counter()
+        logging.info("END make_thumbnails in {} seconds".format(end - start))
     
-    def download_images_producer(self, img_url_list, lock):
-        # in a loop, do each download (but to the staging dir)
-        # try to acquire the lock in a non-blocking way 
-        # if yes, then move all images in the staging dir to the incoming dir
-        # when done, release the lock 
-        pass
-
-    def perform_resizing_consumer(self, lock):
-        # in a loop, try to acquire the lock in a blocking fashion
-        # when the lock is acquired, process the files in the folder
-        # when done, release the lock. time.sleep for 0.1 seconds to 
-        # allow producer acquire lock  
-        pass
-
-    def make_thumbnails_threadsync(self, img_url_list):
-        # create a lock object 
-        # create a new thread, the target is download_images_producer, start it.
-        # create a 2nd thread, the target is perform_resizing_consumer, start it.
-        # wait for both threads to complete
-        pass
